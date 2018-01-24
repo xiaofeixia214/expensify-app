bf9d99d3c20b01ad862710aadfffe4fc74f58ac2
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-01-24 14:17:48 -0800

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,3 +38,24 @@ export const editExpense = (id, updates) => ({
 	id,
 	updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+	type: 'SET_EXPENSES',
+	expenses
+});
+
+export const startSetExpenses = () => {
+	return (dispatch) => {
+		return database.ref('expenses').once('value').then((snapshot) => {
+			const expenses = [];
+			snapshot.forEach((childSnaphot) => {
+				expenses.push({
+					id: childSnaphot.key,
+					...childSnaphot.val()
+				});
+			});
+			dispatch(setExpenses(expenses));
+		});
+	}
+};
