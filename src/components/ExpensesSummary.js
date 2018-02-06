fab9bcd321d3376e97cb1e517d09885e7c56842d
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-02-05 16:48:33 -0800

    show number of hidden expenses in dashboard summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,13 +5,15 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selecctExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, hiddenExpenseCount }) => {
 	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+	const hiddenExpenseWord = hiddenExpenseCount == 1 ? 'expense' : 'expenses';
 	const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
 	return (
 		<div className="page-header">
 			<div className="content-container">
 				<h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal}</span></h1>
+				<h3 className="page-header__sub-title">{hiddenExpenseCount ? `Not showing ${hiddenExpenseCount} ${hiddenExpenseWord} because of filters.` : 'Showing all expenses. No filters in place'}</h3>
 				<div className="page-header__actions">
 					<Link className="button" to="/create">Add Expense</Link>
 				</div>
@@ -24,7 +26,8 @@ const mapStateToProps = (state) => {
 	const visibleExpenses = selectExpenses(state.expenses, state.filters);
 	return {
 		expenseCount: visibleExpenses.length,
-		expensesTotal: selecctExpensesTotal(visibleExpenses)
+		expensesTotal: selecctExpensesTotal(visibleExpenses),
+		hiddenExpenseCount: state.expenses.length - visibleExpenses.length
 	};
 };
 
